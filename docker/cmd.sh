091821c4ea59cc7fdca8abf31a935a1e861d4e80
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-09-05 23:20:10 +0200

    [WIP] test for dump all system env vars - no-jq

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -6,6 +6,6 @@ sleep 10
 
 curl -fsS --retry 10 http://localhost:8080/_ah/health
 
-curl -X POST -H "Accept: application/json" http://localhost:8080/_ah/envs | jq
+curl -X POST -H "Accept: application/json" http://localhost:8080/_ah/envs
 
 
