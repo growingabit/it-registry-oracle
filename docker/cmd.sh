ae2bb991d63fc250f0539d892249a77371953ebf
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-09-05 22:54:25 +0200

    [WIP] test for dump all system env vars - cmd fix - jq

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-java -Xmx2048M -Dfile.encoding=UTF8 -jar /opt/srping-boot-app/target/it-registry-oracle-0.0.1.jar &
+java -Xmx2048M -Dfile.encoding=UTF8 -jar /opt/spring-boot-app/target/it-registry-oracle-0.0.1.jar &
+
+sleep 10
 
 curl -fsS --retry 10 http://localhost:8080/_ah/health
 
-curl -X POST -H "Accept: application/json" http://localhost:8080/_ah/health/dump-envs
+curl -X POST -H "Accept: application/json" http://localhost:8080/_ah/envs | jq
 
 
