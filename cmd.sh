5ce35b2967ea5ffa4c8ce3e160b41862d4ea2ce7
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-09-06 01:42:12 +0200

    [WIP] test for dump all system env vars - ritorni a capo per oraclize

--- cmd.sh
+++ cmd.sh
@@ -4,8 +4,8 @@ java -Xmx2048M -Dfile.encoding=UTF8 -jar /opt/spring-boot-app/target/it-registry
 
 sleep 10
 
+echo -e "\n"
 curl -fsS --retry 10 http://localhost:8080/_ah/health
 
+echo -e "\n"
 curl -X POST -H "Accept: application/json" http://localhost:8080/_ah/envs
-
-
